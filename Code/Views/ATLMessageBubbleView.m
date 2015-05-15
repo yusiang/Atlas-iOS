b6d0eab7ab1804ee1b6256f4f80adfe7fb0f8698
Kabir Mahal <kabirmahal@gmail.com>
2015-05-15 14:54:57 -0700

    Got copy functionality via notifications

--- Code/Views/ATLMessageBubbleView.m
+++ Code/Views/ATLMessageBubbleView.m
@@ -46,6 +46,7 @@ typedef NS_ENUM(NSInteger, ATLBubbleViewContentType) {
 @property (nonatomic) NSLayoutConstraint *imageWidthConstraint;
 @property (nonatomic) MKMapSnapshotter *snapshotter;
 @property (nonatomic) ATLProgressView *progressView;
+//@property (nonatomic, weak) ATLMessageComposeTextView *weakTextView;
 
 @end
 
@@ -238,6 +239,7 @@ typedef NS_ENUM(NSInteger, ATLBubbleViewContentType) {
 
 - (void)menuControllerDisappeared
 {
+    //self.weakTextView.overrideNextResponder = nil;
     [self.longPressMask removeFromSuperview];
     self.longPressMask = nil;
     [[NSNotificationCenter defaultCenter] removeObserver:self];
@@ -254,13 +256,20 @@ typedef NS_ENUM(NSInteger, ATLBubbleViewContentType) {
 {
     if ([recognizer state] == UIGestureRecognizerStateBegan) {
         
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(menuControllerDisappeared)
-                                                     name:UIMenuControllerDidHideMenuNotification
-                                                   object:nil];
+        //[[NSNotificationCenter defaultCenter] postNotificationName:@"testnate" object:self];
         
-        [self becomeFirstResponder];
-        
+//        if ([[UIResponder currentFirstResponder] isKindOfClass:[ATLMessageComposeTextView class]]) {
+//            self.weakTextView = (ATLMessageComposeTextView *)[UIResponder currentFirstResponder];
+//            self.weakTextView.overrideNextResponder = self;
+//            [[NSNotificationCenter defaultCenter] addObserver:self
+//                                                     selector:@selector(menuControllerDisappeared)
+//                                                         name:UIMenuControllerDidHideMenuNotification
+//                                                       object:nil];
+//
+//        } else {
+//            [self becomeFirstResponder];
+//        }
+                
         self.longPressMask = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
         self.longPressMask.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         self.longPressMask.backgroundColor = [UIColor blackColor];
